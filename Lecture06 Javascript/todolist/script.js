0df46b72939de2932ed853265bf9b44973496dda
tanuj_kher <tanujkher15@gmail.com>
2020-01-12 22:21:21 +0530

    Todo List with adding task on pressing Enter

--- Lecture06 Javascript/todolist/script.js
+++ Lecture06 Javascript/todolist/script.js
@@ -2,9 +2,17 @@ window.onload = function () {
     let name = document.getElementById('name')
     let btn = document.getElementById('btn')
     let list = document.getElementById('list')
-    btn.onclick = function () {
+    function addnewtask(){
         let li = document.createElement('li')
-        li.innerText = name.value;
+        li.innerText = name.value
         list.appendChild(li)
     }
+    btn.onclick = function () {
+        addnewtask()
+    }
+    name.addEventListener('keyup',function(event){
+        if (event.keyCode == 13) {
+            addnewtask()
+        }
+    })
 }
