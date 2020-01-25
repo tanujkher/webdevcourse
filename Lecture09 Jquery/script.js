febe2c51653680d68514614bf1938e6db875f900
tanujkher <tanujkher15@gmail.com>
2020-01-25 18:05:57 +0530

    Todo List basic functionality using jQuery

---
+++ Lecture09 Jquery/script.js
@@ -0,0 +1,9 @@
+$(() => {
+    let newtask = $('#newtask')
+    let tasklist = $('#tasklist')
+
+    $('#addtask').click(() => {
+        tasklist.append($('<li>').text(newtask.val()))
+        newtask.val('')
+    })
+})
