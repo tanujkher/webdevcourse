71cd3aeead392c237478b326076f8b6b60529879
tanujkher <tanujkher15@gmail.com>
2020-01-27 17:41:54 +0530

    Achieved Up Functionality in Todo-List with permanent storage

--- Practice/DOM Manipulation/todo-list/script.js
+++ Practice/DOM Manipulation/todo-list/script.js
@@ -15,7 +15,7 @@ window.onload = function(){
             Xbtn.onclick = function(event){
                 event.target.parentElement.remove()
                 let del = event.target.parentElement.innerText
-                del = del.substring(0, del.length - 1)
+                del = del.substring(2)
                 for(let i = 0; i < tasklist.length; i++){
                     if(tasklist[i] == del){
                         tasklist.splice(i, 1)
@@ -24,8 +24,37 @@ window.onload = function(){
                 localStorage.tasks = tasklist.join(',')
             }
             Xbtn.innerText = 'X'
-            item.appendChild(span)
             item.appendChild(Xbtn)
+            let upbtn = document.createElement('button')
+            upbtn.innerText = '^'
+            upbtn.onclick = function(event){
+                event.target.parentElement.parentElement.insertBefore(
+                    event.target.parentElement,
+                    event.target.parentElement.previousElementSibling
+                    )
+                let up = event.target.parentElement.innerText
+                up = up.substring(2)
+                let s1 = 0;
+                for(let i = 0; i < tasklist.length; i++){
+                    if(tasklist[i] == up){
+                        s1 = i
+                    }
+                }
+                if(s1 == 0){
+                    let temp = tasklist[0]
+                    for(let i = 0; i < tasklist.length - 1; i++){
+                        tasklist[i] = tasklist[i + 1]
+                    }
+                    tasklist[tasklist.length - 1] = temp
+                }else{
+                    let temp = tasklist[s1]
+                    tasklist[s1] = tasklist[s1 - 1]
+                    tasklist[s1 - 1] = temp
+                }
+                localStorage.tasks = tasklist.join(',')
+            }
+            item.appendChild(upbtn)
+            item.appendChild(span)
             list.appendChild(item)
         }
     }
