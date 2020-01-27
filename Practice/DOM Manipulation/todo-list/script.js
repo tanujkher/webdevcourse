f6366befddfb62a25dd16d526de1359fe35c8a75
tanujkher <tanujkher15@gmail.com>
2020-01-27 16:13:01 +0530

    Delete and Retain after refresh in sync

--- Practice/DOM Manipulation/todo-list/script.js
+++ Practice/DOM Manipulation/todo-list/script.js
@@ -12,16 +12,18 @@ window.onload = function(){
             let span = document.createElement('span')
             span.innerText = tasklist[i]
             let Xbtn = document.createElement('button')
-            Xbtn.innerText = 'X'
             Xbtn.onclick = function(event){
-                for(let i = 0; i <= tasklist.length - 1; i++){
-                    if(tasklist[i] == event.target.parentElement.span.innerText){
-                        tasklist[i].remove()
+                event.target.parentElement.remove()
+                let del = event.target.parentElement.innerText
+                del = del.substring(0, del.length - 1)
+                for(let i = 0; i < tasklist.length; i++){
+                    if(tasklist[i] == del){
+                        tasklist.splice(i, 1)
                     }
                 }
                 localStorage.tasks = tasklist.join(',')
-                event.target.parentElement.remove()
             }
+            Xbtn.innerText = 'X'
             item.appendChild(span)
             item.appendChild(Xbtn)
             list.appendChild(item)
@@ -32,8 +34,20 @@ window.onload = function(){
         tasklist.push(task.value)
         localStorage.tasks = tasklist.join(',')
         refresh()
+        task.value = ''
     }
 
-    refresh()
+    task.addEventListener('keyup', (event) => {
+        if(event.keyCode == 13){
+            tasklist.push(task.value)
+            localStorage.tasks = tasklist.join(',')
+            refresh()
+            task.value = ''
+        }
+    })
+
+    if(localStorage.tasks != ''){
+        refresh()
+    }
 
 }
