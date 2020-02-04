d4365c5ad7abf71ab8c1d57499eac74fefc88ccd
tanujkher <tanujkher15@gmail.com>
2020-02-04 18:54:15 +0530

    Query and sending HTML page over server

--- Practice/Node-Server-Example/server.js
+++ Practice/Node-Server-Example/server.js
@@ -5,17 +5,28 @@ server.get('/', function(req, res, next){
     res.send("Hello World")
 })
 
-server.get('/greet/:name/:tod', function(req, res, next){
-    let name = req.params.name
+server.get('/greet/:tod', function(req, res, next){
+    let name = req.query.name
     let tod = 'Morning'
     switch(req.params.tod){
-        case 'Evening' : tod = 'Evening'; break;
-        case 'Afternoon' : tod = 'Afternoon'; break;
-        case 'Night' : tod = 'Night'; break;
+        case 'evening' : tod = 'Evening'; break;
+        case 'afternoon' : tod = 'Afternoon'; break;
+        case 'night' : tod = 'Night'; break;
         default : tod = 'Morning';
     }
-    let greeting = 'Good ' + tod + " , " + name
+    let greeting = `Good ${tod} , ${name}`
     res.send(greeting)
+})
+
+server.get('/html', function(req, res, next){
+    res.send(`
+    <html>
+        <body>
+            <h1>This is a heading</h1>
+            <i>Sent a HTML page through server</i>
+        </body>
+    </html>
+    `)
 })
 
 server.listen(2121)
