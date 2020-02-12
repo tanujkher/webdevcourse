999c9a20e00085593b7ffda320c55dc85f58276c
tanujkher <tanujkher15@gmail.com>
2020-02-12 20:17:25 +0530

    Todolist with express delete feature

--- Lecture12 Express_Cont/todolist/server.js
+++ Lecture12 Express_Cont/todolist/server.js
@@ -1,65 +1,88 @@
 const express = require('express')
 const srv = express()
 
-const fs = require('fs').promises
+const fs = require('fs')
 const os = require('os')
 
 let todos = []
 
-srv.get('/', (req, res) => {
-  async function filecopy(){
-    let data = await fs.readFile(__dirname + '/content.txt')
-    todos = data.split(os.EOL)
-    console.log(todos)
-  }
-  filecopy()
-  let list = ''
-  for(let i = 0; i <= todos.length - 1; i++){
-    list = list + '<li>' + todos[i] + `<a href="/delete?id=${i}">X</a>` + '</li>'
-  }
+function read(){
+  return new Promise((resolve, reject) => {
+    fs.readFile(__dirname + '/content.txt', (err, data) => {
+      if(err){
+        reject(err)
+      }else{
+        resolve(data)
+      }
+    })
+  })
+}
 
-  res.send(`
-  <html>
-    <body>
-      <form action='/add'>
-        <input name="task"></input>
-        <input type="submit"></input>
-      </form>
-      <ol>
-        ${list}
-      </ol>
-    </body>
-  </html>
-  `)
+srv.get('/', (req, res) => {
+  read()
+  .then((data) => {
+    if(data.toString()){
+      todos = data.toString().split(os.EOL)
+    }
+    let list = ''
+    for(let i = 0; i <= todos.length - 1; i++){
+      list = list + '<li>' + todos[i] + ` <a href="/delete?id=${i}">X</a>` + '</li>'
+    }
+    res.send(`
+    <html>
+      <body>
+        <form action="/add">
+          <input name="task">
+          <input type="submit">
+        </form>
+        <ol>
+          ${list}
+        </ol>
+      </body>
+    </html>
+    `)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
 })
 
-async function addremdata(){
-  let promise = fs.writeFile(__dirname + '/content.txt', todos.join(os.EOL))
-  await promise.then(() => {
-    console.log('Added')
+function write(data){
+  return new Promise((resolve, reject) => {
+    fs.writeFile(__dirname + '/content.txt', data, (err) => {
+      if(err){
+        reject(err)
+      }else{
+        resolve()
+      }
+    })
   })
 }
 
 srv.get('/add', (req, res) => {
-  if(req.query.task){
-    todos.push(req.query.task)
-    addremdata()
+  todos.push(req.query.task)
+  let content = todos.join(os.EOL)
+  write(content)
+  .then(() => {
     res.redirect('/')
-  }else{
-    res.send('Enter a task')
-  }
+  })
+  .catch((err) => {
+    console.error(err)
+  })
 })
 
 srv.get('/delete', (req, res) => {
-  if(req.query.id >= 0 && req.query.id <= todos.length - 1){
-    todos.splice(req.query.id, 1)
-    addremdata()
+  todos.splice(req.query.id, 1)
+  let content = todos.join(os.EOL)
+  write(content)
+  .then(() => {
     res.redirect('/')
-  }else{
-    res.send('Index not available')
-  }
+  })
+  .catch((err) => {
+    console.error(err)
+  })
 })
 
 srv.listen(5939, () => {
-    console.log('Server started') 
+    console.log('Server started at http://localhost:5939/') 
 })
