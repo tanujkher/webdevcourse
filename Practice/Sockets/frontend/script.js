b5bef7bd6a932990334adee95eefaa0f6a8580d8
tanujkher <tanujkher15@gmail.com>
2020-03-17 11:32:33 +0530

    Chatapp basic sockets

--- Practice/Sockets/frontend/script.js
+++ Practice/Sockets/frontend/script.js
@@ -1,6 +1,19 @@
-window.onload = function() {
-    const socket = io()
-    socket.on('connected', () => {
-        console.log('Connected to ' + socket.id)
-    }) 
-}
+const socket = io()
+socket.on('connected', () => {
+    console.log('Connected to ' + socket.id)
+}) 
+$(() => {
+    let msgbox = $('#msgbox')
+    let sendmsg = $('#sendmsg')
+    let msglist = $('#msglist')
+    
+    sendmsg.click(() => {
+        let msg = msgbox.val()
+        msgbox.val('')
+        socket.emit('send_msg', {msg: msg})
+    })
+
+    socket.on('recv_msg', (data) => {
+        msglist.append($('<li>').text(data.msg))
+    })
+})
--- Practice/Sockets/server.js
+++ Practice/Sockets/server.js
@@ -10,6 +10,9 @@ srv.use('/', express.static(__dirname + '/frontend'))
 io.on('connection', (socket) => {
     console.log('New connection formed from ' + socket.id)
     socket.emit('connected')
+    socket.on('send_msg', (data) => {
+        io.emit('recv_msg', data)
+    })
 })
 
 server.listen(4615, () => {
