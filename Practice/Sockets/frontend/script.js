c2a61af708282f8f1330bd017c6fbd044acb06a0
tanujkher <tanujkher15@gmail.com>
2020-03-19 12:14:51 +0530

    Individual message passing code

--- Practice/Sockets/frontend/script.js
+++ Practice/Sockets/frontend/script.js
@@ -3,17 +3,39 @@ socket.on('connected', () => {
     console.log('Connected to ' + socket.id)
 }) 
 $(() => {
+    let user = ''
+
     let msgbox = $('#msgbox')
     let sendmsg = $('#sendmsg')
     let msglist = $('#msglist')
+
+    let logindiv = $('#login-div')
+    let chatdiv = $('#chat-div')
+    let loginbtn = $('#loginbtn')
+    let curruser = $('#curruser')
+
+    loginbtn.click(() => {
+        console.log('hi')
+        let loginbox = $('#loginbox').val()
+        user = loginbox 
+        curruser.append($('<li>').text(loginbox))
+        socket.emit('login', {
+            user: user      
+        })
+        chatdiv.show()
+        logindiv.hide()
+    })
     
     sendmsg.click(() => {
         let msg = msgbox.val()
         msgbox.val('')
-        socket.emit('send_msg', {msg: msg})
+        socket.emit('send_msg', {
+            user: user,
+            msg: msg
+        })
     })
 
     socket.on('recv_msg', (data) => {
-        msglist.append($('<li>').text(data.msg))
+        msglist.append($('<li>').text(data.user + ' :  ' + data.msg))
     })
 })
--- Practice/Sockets/server.js
+++ Practice/Sockets/server.js
@@ -6,12 +6,24 @@ const http = require('http')
 const server = http.createServer(srv)
 const io = socketio(server)
 
+let usersockets = {}
+
 srv.use('/', express.static(__dirname + '/frontend'))
 io.on('connection', (socket) => {
     console.log('New connection formed from ' + socket.id)
     socket.emit('connected')
+    let found = 0
     socket.on('send_msg', (data) => {
-        io.emit('recv_msg', data)
+        if(data.msg.startsWith('@')){
+            let recipient = data.msg.split(':')[0].substr(1)
+            let rcptSocket = usersockets[recipient]
+            io.to(rcptSocket).emit('recv_msg', data)
+        }else{
+            io.emit('recv_msg', data)
+        }
+    })
+    socket.on('login', (data) => {
+        usersockets[data.user] = socket.id
     })
 })
 
