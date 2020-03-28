f5059131b8cf4880e6170a53435e13a07bb6d67d
tanujkher <tanujkher15@gmail.com>
2020-03-28 20:16:30 +0530

    Music app

--- Practice/Sound_Play_Deploying/public/script.js
+++ Practice/Sound_Play_Deploying/public/script.js
@@ -1,3 +1,12 @@
+var socket = io()
+function play(instId){
+    socket.emit('play', {
+        id: instId
+    })
+}
 $(() => {
-    
+    socket.on('play', (data) => {
+        var instrument = $('#' + data.id)[0]
+        instrument.play()
+    })
 })
--- Practice/Sound_Play_Deploying/server.js
+++ Practice/Sound_Play_Deploying/server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const srv = express()
+const http = require('http')
+const socket = require('socket.io')
 
 const SERVER_PORT = 8728 || process.env.PORT
 
+const server = http.createServer(srv)
+
+const io = socket(server)
+
 srv.use('/', express.static(__dirname + '/public'))
 
-srv.listen(SERVER_PORT, () => {
+io.on('connection', (socket) => {
+    socket.on('play', (data) => {
+        io.emit('play', data)
+    })
+})
+
+server.listen(SERVER_PORT, () => {
     console.log(`Server started at http://localhost:${SERVER_PORT}`)
 })
